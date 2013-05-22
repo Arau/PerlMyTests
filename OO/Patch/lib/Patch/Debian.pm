90e648827f07062df81aceffc4d0942fc40a9f56
Ferran Arau <ferran.arau.castell@gmail.com>
2013-05-22 12:44:26 +0200

    Move files

---
+++ OO/Patch/lib/Patch/Debian.pm
@@ -0,0 +1,55 @@
+package Patch::Debian;
+
+use Moose;
+
+has 'packages' => (
+    is      => 'rw',
+    isa     => 'ArrayRef[Patch::Package]',
+    traits  => ['Array'],
+    default => sub { [] },
+    handles => {
+        add_item  => 'push',
+        next_item => 'shift',
+    },
+    reader  => 'get_packages',
+);
+
+sub update_repo_cmd {
+    return "aptitude update";
+}
+
+sub updates_list_cmd {
+    return "aptitude search --disable-columns -F '%p;%v;%V' ~U";
+}
+
+sub update_packages_cmd {
+    return "aptitude safe-upgrade -y -o APT::Get::AllowUnauthenticated=true";
+}
+
+sub parse_updates_list {
+    my ($self, $list) = @_;
+
+    my @lines = split /\n/, $list;
+    foreach my $line (@lines) {
+        my ($name, $version, $avail) = split /;/, $line;
+        $self->add_item(Patch::Package->new(
+                {
+                    name              => $name,
+                    version           => $version,
+                    available_version => $avail,
+                })
+        );
+    }
+}
+
+sub print_updates {
+    my $self = shift;
+    map {
+        print "Package: $_->get_name ";
+        print "Version: $_->get_version ";
+        print "Available: $_->get_available_version \n";
+    } @$self->packages;
+}
+
+
+1;
